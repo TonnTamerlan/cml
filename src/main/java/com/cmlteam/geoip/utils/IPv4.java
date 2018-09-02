e8d107fd2bb43c50c33261397e80c3fcece5a33b
Alexey Kopylov <tonn.tamerlan@gmail.com>
2018-09-02 19:38:51 +0300

    add methods for non-canonical dot-decimal ip address

--- src/main/java/com/cmlteam/geoip/utils/IPv4.java
+++ src/main/java/com/cmlteam/geoip/utils/IPv4.java
@@ -8,7 +8,13 @@ package com.cmlteam.geoip.utils;
  */
 public class IPv4 {
 	
-	private static final String IPv4_REG_EXP = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}" + 
+    private static final long BIGGEST_IP_ADDRESS = 4_294_967_295L;
+
+    private static final int DIGITS_IN_BYTE = 8;
+	
+	private static final byte DIGIT_IN_BINARY_IP_ADDRESS = 32;
+	
+    private static final String IPv4_REG_EXP = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}" + 
 			"(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
 	
 	private IPv4() {}
@@ -27,14 +33,14 @@ public class IPv4 {
 	}
 	
 	/**
-	 * Convert IP address from dot-deciaml notation to decimal notation
+	 * Convert IP address from dot-decimal notation to decimal notation
 	 * @param ip - string with ip address
 	 * @return IP address in decimal notation
-	 * @throws @{@link IllegalArgumentException} if string with ip is not a IP address in dot-decimal notation
+	 * @throws @{@link IllegalArgumentException} if string is not a IP address in dot-decimal notation
 	 */
-	public static long convertToDecimal(String ip) {
+	public static long fromCanonicalDotDecimalToDecimal(String ip) {
 		if(!isIPv4InDotDecimal(ip)) {
-			String errorMessage = ip + " is not correct argument in convertToDecimal method";
+			String errorMessage = ip + " is not correct argument in convertFromCanonicalDotDecimalToDecimal method";
 			throw new IllegalArgumentException(errorMessage);
 		}
 		String binaryIp = "";
@@ -44,7 +50,7 @@ public class IPv4 {
 			
 			// if converted second and other octets is needed all "zero" digit which must be previously the first "one" digit
 			if (!binaryIp.equals("")) {
-				while (octetBin.length() < 8) {
+				while (octetBin.length() < DIGITS_IN_BYTE) {
 					octetBin = "0" + octetBin;
 				}
 			}
@@ -53,5 +59,83 @@ public class IPv4 {
 		return Long.parseLong(binaryIp, 2);
 	}
 	
+	/**
+	 * Convert IP address in dot-decimal notation (canonical and non-canonical) to decimal notation
+	 * @param ip - string with ip address
+     * @return IP address in decimal notation
+     * @throws @{@link IllegalArgumentException} if string is not a IP address in dot-decimal notation or IP address is incorrect
+     */
+	public static long fromDotDecimalToDecimal(String ip) {
+	    String likeIpAdressRegEx = "(\\d*\\.){0,3}\\d*";
+        if(!ip.matches(likeIpAdressRegEx)) {
+	        String errorMessage = ip + " is not correct ip address";
+            throw new IllegalArgumentException(errorMessage);
+	    }
+	    String binaryIp = "";
+	    
+	    String[] octets = ip.split("\\.");
+	    for (int i = 0; i < octets.length; i++) {
+            String octetBin = Long.toBinaryString(Long.parseLong(octets[i]));
+            
+            
+            // if converted second and other octets except last, is needed all "zero" digit which must be previously the first "one" digit
+            if (i != 0 && i < octets.length - 1) {
+                if(octetBin.length() > DIGITS_IN_BYTE) {
+                    String errorMessage = ip + " is not correct ip address";
+                    throw new IllegalArgumentException(errorMessage);
+                }
+                while (octetBin.length() < DIGITS_IN_BYTE) {
+                    octetBin = "0" + octetBin;
+                }
+            }
+            // fill last octet by "zero" digit
+            if (i == octets.length - 1) {
+                int octetsInCanonicalIpAddress = 4;
+                int numberOfDigit = DIGITS_IN_BYTE + DIGITS_IN_BYTE * (octetsInCanonicalIpAddress - octets.length);
+                if(octetBin.length() > numberOfDigit) {
+                    String errorMessage = ip + " is not correct ip address";
+                    throw new IllegalArgumentException(errorMessage);
+                }
+                while (octetBin.length() < numberOfDigit) {
+                    octetBin = "0" + octetBin;
+                }
+            }
+            binaryIp += octetBin;
+            if(binaryIp.length() > DIGIT_IN_BINARY_IP_ADDRESS) {
+                String errorMessage = ip + " is not correct ip address";
+                throw new IllegalArgumentException(errorMessage);
+            }
+        }
+	    
+	    long ipAddresInDecimal = Long.parseLong(binaryIp, 2);
+	    
+	    if(ipAddresInDecimal > BIGGEST_IP_ADDRESS) {
+	        String errorMessage = ip + " is not correct ip address";
+            throw new IllegalArgumentException(errorMessage);
+	    }
+	    return ipAddresInDecimal;
+	}
+	
+	public static String fromDecimalToDotDecimal(long ipDecimal) {
+	    if(ipDecimal < 0 || ipDecimal > BIGGEST_IP_ADDRESS) {
+	        String errorMessage = ipDecimal + " is not IPv4 address";
+	        throw new IllegalArgumentException(errorMessage);
+	    }
+	    String ipBinary = Long.toBinaryString(ipDecimal);
+	    
+	    return binaryToDotDecimal(ipBinary);
+	}
+
+    private static String binaryToDotDecimal(String ipBinary) {
+        while(ipBinary.length() < DIGIT_IN_BINARY_IP_ADDRESS) {
+            ipBinary = "0" + ipBinary;
+        }
+        int firstOctet = Integer.parseInt(ipBinary.substring(0, 8), 2);
+        int secondOctet = Integer.parseInt(ipBinary.substring(8, 16), 2);
+        int thirdOctet = Integer.parseInt(ipBinary.substring(16, 24), 2);
+        int fourthOctet = Integer.parseInt(ipBinary.substring(24, 32), 2);
+               
+        return firstOctet + "." + secondOctet + "." + thirdOctet + "." + fourthOctet;
+    }
 
 }
--- src/test/java/com/cmlteam/geoip/utils/IPv4Test.java
+++ src/test/java/com/cmlteam/geoip/utils/IPv4Test.java
@@ -35,26 +35,107 @@ class IPv4Test {
 	}
 
 	@Nested
-	@DisplayName("Test convertToDecimal method")
-	class ConvertToDecimalTest{
+	@DisplayName("Test fromCanonicalDotDecimalToDecimal method")
+	class FromCanonicalDotDecimalToDecimalTest{
 		
 		@Test
 		@DisplayName("Test with correct parameters")
-		void testConvertToDecimalWithCorrectParameters() {
-			assertEquals(IPv4.convertToDecimal("0.0.0.0"), 0);
-			assertEquals(IPv4.convertToDecimal("1.1.1.1"), 16843009);
-			assertEquals(IPv4.convertToDecimal("12.12.255.255"), 202178559);
+		void testFromCanonicalDotDecimalToDecimalWithCorrectParameters() {
+			assertEquals(IPv4.fromCanonicalDotDecimalToDecimal("0.0.0.0"), 0);
+			assertEquals(IPv4.fromCanonicalDotDecimalToDecimal("1.1.1.1"), 16843009);
+			assertEquals(IPv4.fromCanonicalDotDecimalToDecimal("12.12.255.255"), 202178559);
 		}
 		
 		@Test
 		@DisplayName("Test with noncorrect parameters")
-		void testConvertToDecimalWithNonCorrectParameters() {
+		void testFromCanonicalDotDecimalToDecimalWithNonCorrectParameters() {
 			String nonCorrectArgument = "noncorrect";
 			Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
-				IPv4.convertToDecimal(nonCorrectArgument);
+				IPv4.fromCanonicalDotDecimalToDecimal(nonCorrectArgument);
 			});
-			assertEquals(nonCorrectArgument + " is not correct argument in convertToDecimal method", exception.getMessage());
+			assertEquals(nonCorrectArgument + " is not correct argument in convertFromCanonicalDotDecimalToDecimal method", exception.getMessage());
 		}
+	}
+	
+	@Nested
+	@DisplayName("Test fromDotDecimalToDecimal method")
+	class FromDotDecimalToDecimalTest {
+	    
+	    @Test
+	    @DisplayName("Test with correct parameters")
+	    void testFromDotDecimalToDecimalWithCorrectParameters() {
+	        assertEquals(134_744_072, IPv4.fromDotDecimalToDecimal("8.8.8.8"));
+	        assertEquals(4_294_967_295L, IPv4.fromDotDecimalToDecimal("255.255.255.255"));
+	        assertEquals(4_294_902_015L, IPv4.fromDotDecimalToDecimal("255.255.255"));
+	        assertEquals(134_218_984, IPv4.fromDotDecimalToDecimal("8.1256"));
+	        assertEquals(568_579, IPv4.fromDotDecimalToDecimal("568579"));
+	    }
+	    
+	    @Test
+	    @DisplayName("Test with noncorrect parameters")
+	    void testFromDotDecimalToDecimalWithNonCorrectParameters(){
+	        String withAlfabet = "noncorrect";
+	        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+	           IPv4.fromDotDecimalToDecimal(withAlfabet); 
+	        });
+	        assertEquals(withAlfabet + " is not correct ip address", exception.getMessage());
+	        
+	        String fiveOctets = "125365.12222.12210.21233.12122";
+            exception = assertThrows(IllegalArgumentException.class, () -> {
+               IPv4.fromDotDecimalToDecimal(fiveOctets); 
+            });
+            assertEquals(fiveOctets + " is not correct ip address", exception.getMessage());
+            
+            String oneCharacter = "1253.t255.566.6215";
+            exception = assertThrows(IllegalArgumentException.class, () -> {
+               IPv4.fromDotDecimalToDecimal(oneCharacter); 
+            });
+            assertEquals(oneCharacter + " is not correct ip address", exception.getMessage());
+            
+            String wrongIP_1 = "256.255.255.255";
+            exception = assertThrows(IllegalArgumentException.class, () -> {
+               IPv4.fromDotDecimalToDecimal(wrongIP_1); 
+            });
+            assertEquals(wrongIP_1 + " is not correct ip address", exception.getMessage());
+         
+            String wrongIP_2 = "12.242332432";
+            exception = assertThrows(IllegalArgumentException.class, () -> {
+                IPv4.fromDotDecimalToDecimal(wrongIP_2);
+            });
+            assertEquals(wrongIP_2 + " is not correct ip address", exception.getMessage());
+	    }
+	    
+	}
+	
+	@Nested
+	@DisplayName("Test fromDecimalToDotDecimal method")
+	class FromDecimalToDotDecimalTest {
+	    
+	    private static final long BIGGEST_IP_ADDRESS = 4_294_967_295L;
+
+        @Test
+	    @DisplayName("Test with correct parameters")
+	    void testFromDecimalToDotDecimalWithCorrectParameters() {
+	        assertEquals("0.0.0.0", IPv4.fromDecimalToDotDecimal(0));
+	        assertEquals("125.125.125.125", IPv4.fromDecimalToDotDecimal(2_105_376_125L));
+	        assertEquals("255.255.255.255", IPv4.fromDecimalToDotDecimal(BIGGEST_IP_ADDRESS));
+	    }
+	    
+	    @Test
+        @DisplayName("Test with noncorrect parameters")
+        void testFromDecimalToDotDecimalWithNonCorrectParameters() {
+	        long lessZero = -1;
+	        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+	           IPv4.fromDecimalToDotDecimal(lessZero); 
+	        });
+	        assertEquals(lessZero + " is not IPv4 address", exception.getMessage());
+	        
+	        long largerBiggestIpAddress = BIGGEST_IP_ADDRESS + 1;
+	        exception = assertThrows(IllegalArgumentException.class, () -> {
+	           IPv4.fromDecimalToDotDecimal(largerBiggestIpAddress); 
+	        });
+	        assertEquals(largerBiggestIpAddress + " is not IPv4 address", exception.getMessage());
+	    }
 	}
 
 }
